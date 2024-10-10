52886287ec612efff5332e16ff73ed2fd7ddf481
avern2024 <avern2020@icloud.com>
2024-10-10 18:47:56 +0700

    Add files via upload

---
+++ module_2.3.py
@@ -0,0 +1,9 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index_ = 0
+while index_ < len(my_list):
+    num = (my_list[index_])
+    if num > 0:
+        print(num)
+    elif num < 0:
+        break
+    index_ += 1
